2bde0949c9e040d4b3b79f17b104e10dfb06fb5a
Kelian9995 <nsokovnin@gmail.com>
2022-07-22 21:24:54 +0300

    added button back

--- GeoQuiz/app/src/main/java/com/bignerdranch/android/geomain/MainActivity.kt
+++ GeoQuiz/app/src/main/java/com/bignerdranch/android/geomain/MainActivity.kt
@@ -6,13 +6,15 @@ import android.view.Gravity
 import android.view.View
 import android.widget.AutoCompleteTextView
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
-    private lateinit var nextButton:Button
+    private lateinit var nextButton: ImageButton
+    private lateinit var prevButton: ImageButton
     private lateinit var questionTextView:TextView
     private val questionBank = listOf(
         Question(R.string.question_australia,true),
@@ -37,6 +39,7 @@ class MainActivity : AppCompatActivity() {
             findViewById(R.id.false_button)
 
         nextButton = findViewById(R.id.next_button)
+        prevButton = findViewById(R.id.prev_button)
         questionTextView = findViewById(R.id.question_text_view)
         falseButton.setOnClickListener { view: View ->
            checkAnswer(false)
@@ -46,13 +49,28 @@ class MainActivity : AppCompatActivity() {
                     questionBank.size
             updateQuestion()
         }
-        updateQuestion()
+        prevButton.setOnClickListener {
+            if (currentIndex > 0) {
+                currentIndex = (currentIndex - 1)
+                updateQuestion()
+            } else {
+                currentIndex = questionBank.size - 1
+                updateQuestion()
+            }
+        }
+
+        questionTextView.setOnClickListener {
+            currentIndex = (currentIndex + 1) % questionBank.size
+            updateQuestion()
+        }
+
         }
     private fun updateQuestion() {
 
 
         val questionTextResId = questionBank[currentIndex].textResId
         questionTextView.setText(questionTextResId)
+
     }
     private fun checkAnswer(userAnswer:Boolean) {
         val correctAnswer = questionBank[currentIndex].answer
